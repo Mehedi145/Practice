1b5966714584aabc978d2cdcaeeac754c1fac1a7
Mehedi Hasan <mehedihasanli809@gmail.com>
2022-11-10 11:47:59 -0900

    commit-3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let fName = 'Mehedi';
+let lName = 'Hasan';
+let fullName;
+
+if (fName || lName) {
+    fullName = fName, lName;
+} else {
+    fullName = 'Mehedi Hasan';
+
+}
+
+console.log(fullName);
